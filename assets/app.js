2a62f15c1f2a09a6e53d95113d79116f2a59be82
AxelDrouillard <axel.drouillard@gmail.com>
2022-05-24 09:57:49 +0200

    webpack

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,17 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
+
+// assets/app.js
+
+// returns the final, public path to this file
+// path is relative to this file - e.g. assets/images/logo.png
+import logoPath from '../images/logo.png';
+
+let html = `<img src="${logoPath}" alt="ACME logo">`;
+
+console.log('Hello Webpack Encore !')
